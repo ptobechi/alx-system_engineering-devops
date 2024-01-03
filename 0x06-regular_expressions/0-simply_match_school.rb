db93dfc4ad163015e421f3073e1c079b79536efe
ptobechi <ptobechii@gmail.com>
2024-01-03 15:50:58 -0600

    a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,11 +2,22 @@
 
 require 'oniguruma'
 
-def match_regex(str)
-  regex = Oniguruma::ORegexp.new('School')
-  matches = regex.match(str)
+def match_regex(input_str)
+  # Define your regular expression pattern here
+  regex_pattern = Oniguruma::ORegexp.new('School')
+
+  # Perform the match
+  matches = regex_pattern.match(input_str)
+
+  # Output the matches
   puts matches
 end
 
-match_regex(ARGV[0])
+# Check if a command-line argument is provided
+if ARGV.empty?
+  puts 'Please provide a string as a command-line argument.'
+else
+  # Call the match_regex function with the provided argument
+  match_regex(ARGV[0])
+end
 
