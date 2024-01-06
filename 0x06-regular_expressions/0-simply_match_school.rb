d85766a028356fe31dc6620dd3f484f34c3771c3
ptobechi <ptobechii@gmail.com>
2024-01-06 03:28:20 -0600

    correction

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -9,15 +9,7 @@ def match_regex(input_str)
   # Perform the match
   matches = regex_pattern.match(input_str)
 
-  # Output the matches
-  puts matches
-end
-
-# Check if a command-line argument is provided
-if ARGV.empty?
-  puts 'Please provide a string as a command-line argument.'
-else
-  # Call the match_regex function with the provided argument
-  match_regex(ARGV[0])
+  # Output the matched string
+  puts matches[0] if matches
 end
 
