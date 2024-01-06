a600bc4614f5063ecfd3da79e85705b6d665168e
ptobechi <ptobechii@gmail.com>
2024-01-06 14:58:13 -0600

    Networking Basics 2

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,3 @@
 #!/usr/bin/env ruby
-
-require 'oniguruma'
-
-def match_regex(input_str)
-  # Define your regular expression pattern here
-  regex_pattern = Oniguruma::ORegexp.new('School')
-
-  # Perform the match
-  matches = regex_pattern.match(input_str)
-
-  # Output all matched strings
-  matches.to_a.each { |match| puts match } if matches
-end
+puts ARGV[0].scan(/School/).join
 
