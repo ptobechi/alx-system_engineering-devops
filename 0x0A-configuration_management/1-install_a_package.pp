163edad7a4b103a9e20dfb4306ee16cc3faa680f
ptobechi <ptobechii@gmail.com>
2024-02-23 12:36:19 -0600

    puppet installation manifest for flask and pip

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Puppet Manifest to Install Flask
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.x/dist-packages/flask',
+  require => Package['python3-pip'],
+}
+
