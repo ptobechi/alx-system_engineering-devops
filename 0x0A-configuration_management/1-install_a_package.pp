021b6cc70f87ab97274ef41ee25d2497cbb4ea36
ptobechi <ptobechii@gmail.com>
2024-02-24 03:35:24 -0600

    correctiions to meet requirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,23 +1,6 @@
-# Puppet Manifest to Install Flask with Dependencies
-package { 'python3-pip':
-  ensure => 'installed',
+# install flask from pip3
+package {
+  'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-package { 'libffi-dev':
-  ensure => 'installed',
-  # Add other dependencies if needed
-}
-
-exec { 'install_flask_dependencies':
-  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
-  path    => ['/usr/bin'],
-  require => [Package['python3-pip'], Package['libffi-dev']],
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  creates => '/usr/local/lib/python3.x/dist-packages/Flask',
-  require => Exec['install_flask_dependencies'],
-}
-
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,23 +1,5 @@
-# Puppet Manifest to Kill a Process
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  notify      => Exec['check_process'],
+# kills a process named killmenow.
+exec {
+  'pkill -f killmenow':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
-
-exec { 'check_process':
-  command     => 'pgrep -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  onlyif      => 'test "$(pgrep -f killmenow)" != ""',
-  notify      => Exec['process_result'],
-}
-
-exec { 'process_result':
-  command     => 'echo OK',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  onlyif      => 'test "$(pgrep -f killmenow)" = ""',
-}
-
