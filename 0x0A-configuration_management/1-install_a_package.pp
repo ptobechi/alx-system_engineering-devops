62da958a75b83915f6293b6e5117ac95aa3bc805
ptobechi <ptobechii@gmail.com>
2024-02-24 03:21:43 -0600

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,23 @@
-# Puppet Manifest to Install Flask
+# Puppet Manifest to Install Flask with Dependencies
 package { 'python3-pip':
   ensure => 'installed',
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+package { 'libffi-dev':
+  ensure => 'installed',
+  # Add other dependencies if needed
+}
+
+exec { 'install_flask_dependencies':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
   path    => ['/usr/bin'],
-  creates => '/usr/local/lib/python3.x/dist-packages/flask',
-  require => Package['python3-pip'],
+  require => [Package['python3-pip'], Package['libffi-dev']],
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.x/dist-packages/Flask',
+  require => Exec['install_flask_dependencies'],
 }
 
