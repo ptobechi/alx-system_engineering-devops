e1f4ce629cd84a97a6e8d0b85fd685835e3bb33a
ptobechi <ptobechii@gmail.com>
2024-02-24 03:26:11 -0600

    corrections

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,16 +1,9 @@
 # Puppet Manifest to Kill a Process
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
+  command     => 'pkill -f "killmenow"',
   path        => ['/bin', '/usr/bin'],
   refreshonly => true,
-  notify      => Exec['check_process'],
-}
-
-exec { 'check_process':
-  command     => 'pgrep -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  onlyif      => 'test "$(pgrep -f killmenow)" != ""',
+  onlyif      => 'pgrep -f "killmenow"',
   notify      => Exec['process_result'],
 }
 
