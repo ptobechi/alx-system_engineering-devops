5b6eb18299869476a1404e5a7f1befd9eb16dc05
ptobechi <ptobechii@gmail.com>
2024-02-24 03:24:15 -0600

    corrections

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -3,5 +3,20 @@ exec { 'killmenow':
   command     => 'pkill -f killmenow',
   path        => ['/bin', '/usr/bin'],
   refreshonly => true,
+  notify      => Exec['check_process'],
+}
+
+exec { 'check_process':
+  command     => 'pgrep -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+  onlyif      => 'test "$(pgrep -f killmenow)" != ""',
+  notify      => Exec['process_result'],
+}
+
+exec { 'process_result':
+  command     => 'echo OK',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
 }
 
